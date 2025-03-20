9087f9382e8698f880e67464ed05f68477a9ac5e
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:41 +0300

    add network byte order to port and tests

--- core/src/ip/port.cpp
+++ core/src/ip/port.cpp
@@ -1,9 +1,18 @@
 #include "ip/port.hpp"
 
+#include <arpa/inet.h>
+
 namespace core::ip {
 
 port::port(std::uint16_t port) noexcept : port_(port) {}
 
+port::port(std::uint16_t port, network_byte_order_t) noexcept
+    : port_(ntohs(port)) {}
+
 std::uint16_t port::get_bytes() const noexcept { return port_; }
+
+std::uint16_t port::get_bytes(network_byte_order_t) const noexcept {
+  return htons(port_);
+}
 
 }  // namespace core::ip
