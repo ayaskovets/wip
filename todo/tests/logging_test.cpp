f3b22aa2d8ef9fd1418baca24cf4fb7de606ba44
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:36 +0300

    add tests

---
+++ todo/tests/logging_test.cpp
@@ -0,0 +1,61 @@
+#include "logging/logging.hpp"
+
+#include <gtest/gtest.h>
+
+namespace tests::logging {
+
+namespace logging = todo::logging;
+
+TEST(todo, writer) {
+  std::string buffer;
+  const logging::logger logger(
+      logging::level::kInfo, logging::renderer::kNoop(),
+      logging::writer(
+          [&buffer]([[maybe_unused]] logging::level level,
+                    std::string_view message) { buffer = message; }));
+
+  logger(logging::level::kInfo, "{}", 42);
+  EXPECT_EQ(buffer, "42");
+}
+
+TEST(todo, skip) {
+  std::string buffer;
+  const logging::logger logger(
+      logging::level::kWarn, logging::renderer::kNoop(),
+      logging::writer([&buffer]([[maybe_unused]] logging::level level,
+                                [[maybe_unused]] std::string_view message) {
+        buffer.resize(buffer.size() + 1);
+      }));
+
+  logger(logging::level::kDebug, "message");
+  EXPECT_TRUE(buffer.empty());
+
+  logger(logging::level::kInfo, "message");
+  EXPECT_TRUE(buffer.empty());
+
+  logger(logging::level::kWarn, "message");
+  EXPECT_EQ(buffer.size(), 1);
+
+  logger(logging::level::kError, "message");
+  EXPECT_EQ(buffer.size(), 2);
+}
+
+TEST(todo, renderer) {
+  std::string buffer;
+  const logging::logger logger(
+      logging::level::kInfo,
+      logging::renderer([]([[maybe_unused]] logging::level level,
+                           std::string_view fmt,
+                           std::format_args args) -> std::string {
+        return std::vformat(fmt, std::move(args)) +
+               std::vformat(fmt, std::move(args));
+      }),
+      logging::writer(
+          [&buffer]([[maybe_unused]] logging::level level,
+                    std::string_view message) { buffer = message; }));
+
+  logger(logging::level::kInfo, "{}", 42);
+  EXPECT_EQ(buffer, "4242");
+}
+
+}  // namespace tests::logging
