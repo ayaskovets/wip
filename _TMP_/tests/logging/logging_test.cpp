e042c52f412ed4a7fe577afdc527d459ce1278f7
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:30 +0300

    refactor tests

--- _TMP_/tests/logging/logging_test.cpp
+++ _TMP_/tests/logging/logging_test.cpp
@@ -4,54 +4,64 @@
 
 namespace tests::logging {
 
+TEST(_TMP__logging, size) {
+  static_assert(sizeof(_TMP_::logging::logger) == 48);
+  static_assert(alignof(_TMP_::logging::logger) == 8);
+}
+
 TEST(_TMP__logging, writer) {
   std::string buffer;
-  const _TMP_::logging::logger logger(
-      _TMP_::logging::level::kInfo, _TMP_::logging::renderer::kNoop(),
-      _TMP_::logging::writer(
-          [&buffer]([[maybe_unused]] _TMP_::logging::level level,
-                    std::string_view message) { buffer = message; }));
 
-  logger(_TMP_::logging::level::kInfo, "{}", 42);
+  const auto level = _TMP_::logging::level::kInfo;
+  const auto renderer = _TMP_::logging::renderer::kNoop();
+  const auto writer = _TMP_::logging::writer(
+      [&buffer]([[maybe_unused]] _TMP_::logging::level level,
+                std::string_view message) { buffer = message; });
+  const auto logger = _TMP_::logging::logger(level, renderer, writer);
+
+  logger(level, "{}", 42);
   EXPECT_EQ(buffer, "42");
 }
 
 TEST(_TMP__logging, skip) {
-  std::string buffer;
-  const _TMP_::logging::logger logger(
-      _TMP_::logging::level::kWarn, _TMP_::logging::renderer::kNoop(),
-      _TMP_::logging::writer(
-          [&buffer]([[maybe_unused]] _TMP_::logging::level level,
+  std::size_t logs_count = 0;
+
+  const auto level = _TMP_::logging::level::kWarn;
+  const auto renderer = _TMP_::logging::renderer::kNoop();
+  const auto writer = _TMP_::logging::writer(
+      [&logs_count]([[maybe_unused]] _TMP_::logging::level level,
                     [[maybe_unused]] std::string_view message) {
-            buffer.resize(buffer.size() + 1);
-          }));
+        logs_count += 1;
+      });
+  const auto logger = _TMP_::logging::logger(level, renderer, writer);
 
   logger(_TMP_::logging::level::kDebug, "message");
-  EXPECT_TRUE(buffer.empty());
+  EXPECT_EQ(logs_count, 0);
 
   logger(_TMP_::logging::level::kInfo, "message");
-  EXPECT_TRUE(buffer.empty());
+  EXPECT_EQ(logs_count, 0);
 
   logger(_TMP_::logging::level::kWarn, "message");
-  EXPECT_EQ(buffer.size(), 1);
+  EXPECT_EQ(logs_count, 1);
 
   logger(_TMP_::logging::level::kError, "message");
-  EXPECT_EQ(buffer.size(), 2);
+  EXPECT_EQ(logs_count, 2);
 }
 
 TEST(_TMP__logging, renderer) {
   std::string buffer;
-  const _TMP_::logging::logger logger(
-      _TMP_::logging::level::kInfo,
+
+  const auto level = _TMP_::logging::level::kInfo;
+  const auto renderer =
       _TMP_::logging::renderer([]([[maybe_unused]] _TMP_::logging::level level,
-                                  std::string_view fmt,
-                                  std::format_args args) -> std::string {
+                                  std::string_view fmt, std::format_args args) {
         return std::vformat(fmt, std::move(args)) +
                std::vformat(fmt, std::move(args));
-      }),
-      _TMP_::logging::writer(
-          [&buffer]([[maybe_unused]] _TMP_::logging::level level,
-                    std::string_view message) { buffer = message; }));
+      });
+  const auto writer = _TMP_::logging::writer(
+      [&buffer]([[maybe_unused]] _TMP_::logging::level level,
+                std::string_view message) { buffer = message; });
+  const auto logger = _TMP_::logging::logger(level, renderer, writer);
 
   logger(_TMP_::logging::level::kInfo, "{}", 42);
   EXPECT_EQ(buffer, "4242");
