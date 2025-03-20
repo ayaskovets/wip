d12ee9c0506bc78a9b9a5b2d6114388a574ed98e
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:39 +0300

    restructure benchmarks

--- _TMP_/benchmarks/ip/address_benchmark.cpp
+++ _TMP_/benchmarks/ip/address_benchmark.cpp
@@ -8,6 +8,7 @@ void BM_ip4_from_bytes(benchmark::State& state) {
     benchmark::DoNotOptimize(_TMP_::ip::address(bytes));
   }
 }
+BENCHMARK(BM_ip4_from_bytes);
 
 void BM_ip6_from_bytes(benchmark::State& state) {
   const std::vector<std::uint8_t> bytes{255, 255, 255, 255, 255, 255, 255, 255,
@@ -16,6 +17,7 @@ void BM_ip6_from_bytes(benchmark::State& state) {
     benchmark::DoNotOptimize(_TMP_::ip::address(std::move(bytes)));
   }
 }
+BENCHMARK(BM_ip6_from_bytes);
 
 void BM_ip4_from_string(benchmark::State& state) {
   const std::string string("127.0.0.1");
@@ -23,6 +25,7 @@ void BM_ip4_from_string(benchmark::State& state) {
     benchmark::DoNotOptimize(_TMP_::ip::address(string));
   }
 }
+BENCHMARK(BM_ip4_from_string);
 
 void BM_ip6_from_string(benchmark::State& state) {
   const std::string string("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
@@ -30,6 +33,7 @@ void BM_ip6_from_string(benchmark::State& state) {
     benchmark::DoNotOptimize(_TMP_::ip::address(std::move(string)));
   }
 }
+BENCHMARK(BM_ip6_from_string);
 
 void BM_ip4_as_string(benchmark::State& state) {
   const _TMP_::ip::address address("127.0.0.1");
@@ -37,6 +41,7 @@ void BM_ip4_as_string(benchmark::State& state) {
     benchmark::DoNotOptimize(address.as_string());
   }
 }
+BENCHMARK(BM_ip4_as_string);
 
 void BM_ip6_as_string(benchmark::State& state) {
   const auto address =
@@ -45,10 +50,4 @@ void BM_ip6_as_string(benchmark::State& state) {
     benchmark::DoNotOptimize(address.as_string());
   }
 }
-
-BENCHMARK(BM_ip4_from_bytes);
-BENCHMARK(BM_ip6_from_bytes);
-BENCHMARK(BM_ip4_from_string);
-BENCHMARK(BM_ip6_from_string);
-BENCHMARK(BM_ip4_as_string);
 BENCHMARK(BM_ip6_as_string);
