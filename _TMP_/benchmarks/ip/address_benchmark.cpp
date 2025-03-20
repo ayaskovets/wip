aa170bf33e6f95e092b26fa170d1965104054ca5
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:38 +0300

    reorder address benchmark

--- _TMP_/benchmarks/ip/address_benchmark.cpp
+++ _TMP_/benchmarks/ip/address_benchmark.cpp
@@ -11,20 +11,6 @@ void BM_ip4_from_bytes(benchmark::State& state) {
   }
 }
 
-void BM_ip4_from_string(benchmark::State& state) {
-  const std::string string("127.0.0.1");
-  for (const auto _ : state) {
-    benchmark::DoNotOptimize(_TMP_::ip::address(string));
-  }
-}
-
-void BM_ip4_as_string(benchmark::State& state) {
-  const _TMP_::ip::address address("127.0.0.1");
-  for (const auto _ : state) {
-    benchmark::DoNotOptimize(address.as_string());
-  }
-}
-
 void BM_ip6_from_bytes(benchmark::State& state) {
   const std::vector<std::uint8_t> bytes{255, 255, 255, 255, 255, 255, 255, 255,
                                         255, 255, 255, 255, 255, 255, 255, 255};
@@ -33,10 +19,24 @@ void BM_ip6_from_bytes(benchmark::State& state) {
   }
 }
 
+void BM_ip4_from_string(benchmark::State& state) {
+  const std::string string("127.0.0.1");
+  for (const auto _ : state) {
+    benchmark::DoNotOptimize(_TMP_::ip::address(string));
+  }
+}
+
 void BM_ip6_from_string(benchmark::State& state) {
   const std::string string("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
   for (const auto _ : state) {
     benchmark::DoNotOptimize(_TMP_::ip::address(std::move(string)));
+  }
+}
+
+void BM_ip4_as_string(benchmark::State& state) {
+  const _TMP_::ip::address address("127.0.0.1");
+  for (const auto _ : state) {
+    benchmark::DoNotOptimize(address.as_string());
   }
 }
 
@@ -51,8 +51,8 @@ void BM_ip6_as_string(benchmark::State& state) {
 }  // namespace benchmarks::ip
 
 BENCHMARK(benchmarks::ip::BM_ip4_from_bytes);
-BENCHMARK(benchmarks::ip::BM_ip4_from_string);
-BENCHMARK(benchmarks::ip::BM_ip4_as_string);
 BENCHMARK(benchmarks::ip::BM_ip6_from_bytes);
+BENCHMARK(benchmarks::ip::BM_ip4_from_string);
 BENCHMARK(benchmarks::ip::BM_ip6_from_string);
+BENCHMARK(benchmarks::ip::BM_ip4_as_string);
 BENCHMARK(benchmarks::ip::BM_ip6_as_string);
