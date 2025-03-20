6d50e179c3e6b79eafbf3071fcf95930296b2857
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:37 +0300

    refactor tests using test fixtures and parameters

--- _TMP_/tests/ip_test.cpp
+++ _TMP_/tests/ip_test.cpp
@@ -5,18 +5,19 @@
 
 namespace tests::logging {
 
-namespace ip = _TMP_::ip;
+class _TMP__ip_address
+    : public ::testing::TestWithParam<std::tuple<
+          std::vector<std::uint8_t>, std::string, _TMP_::ip::version>> {};
 
-TEST(_TMP__ip, ip4_address_constructor) {
-  const std::vector<std::uint8_t> bytes{255, 255, 255, 255};
-  const std::string string("255.255.255.255");
+TEST_P(_TMP__ip_address, construction) {
+  const auto [bytes, string, version] = GetParam();
 
-  const auto address1 = ip::address(bytes);
-  const auto address2 = ip::address(string);
+  const auto address1 = _TMP_::ip::address(bytes);
+  const auto address2 = _TMP_::ip::address(string);
 
   EXPECT_EQ(address1.as_bytes(), bytes);
   EXPECT_EQ(address1.as_string(), string);
-  EXPECT_EQ(address1.version(), ip::version::kIpV4);
+  EXPECT_EQ(address1.version(), version);
 
   EXPECT_EQ(address1.as_bytes(), address2.as_bytes());
   EXPECT_EQ(address1.as_string(), address2.as_string());
@@ -24,30 +25,24 @@ TEST(_TMP__ip, ip4_address_constructor) {
   EXPECT_EQ(address1, address2);
 }
 
-TEST(_TMP__ip, ip6_address_constructor) {
-  const std::vector<std::uint8_t> bytes{255, 255, 255, 255, 255, 255, 255, 255,
-                                        255, 255, 255, 255, 255, 255, 255, 255};
-  const std::string string("ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
-
-  const auto address1 = ip::address(bytes);
-  const auto address2 = ip::address(string);
-
-  EXPECT_EQ(address1, address2);
-  EXPECT_EQ(address1.as_bytes(), bytes);
-  EXPECT_EQ(address1.as_string(), string);
-  EXPECT_EQ(address1.version(), ip::version::kIpV6);
-
-  EXPECT_EQ(address1.as_bytes(), address2.as_bytes());
-  EXPECT_EQ(address1.as_string(), address2.as_string());
-  EXPECT_EQ(address1.version(), address2.version());
-  EXPECT_EQ(address1, address2);
-}
+INSTANTIATE_TEST_SUITE_P(
+    _TMP__ip, _TMP__ip_address,
+    ::testing::Values(
+        std::make_tuple(std::vector<std::uint8_t>{255, 255, 255, 255},
+                        "255.255.255.255", _TMP_::ip::version::kIpV4),
+        std::make_tuple(std::vector<std::uint8_t>{255, 255, 255, 255, 255, 255,
+                                                  255, 255, 255, 255, 255, 255,
+                                                  255, 255, 255, 255},
+                        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff",
+                        _TMP_::ip::version::kIpV6)));
 
 TEST(_TMP__ip, resolve_localhost) {
-  const auto addresses = ip::resolve("localhost", ip::protocol::kTcp);
+  const auto addresses =
+      _TMP_::ip::resolve("localhost", _TMP_::ip::protocol::kTcp);
 
-  EXPECT_EQ(addresses, (std::vector<ip::address>{ip::address("::1"),
-                                                 ip::address("127.0.0.1")}));
+  EXPECT_EQ(addresses,
+            (std::vector<_TMP_::ip::address>{_TMP_::ip::address("::1"),
+                                             _TMP_::ip::address("127.0.0.1")}));
 }
 
 }  // namespace tests::logging
