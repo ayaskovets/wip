be547e86e0046284f5714e12f12a6c62c5db52a7
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:30 +0300

    add formatting for ip version

--- _TMP_/include/ip/version.hpp
+++ _TMP_/include/ip/version.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <format>
 
 namespace _TMP_::ip {
 
@@ -10,3 +11,22 @@ enum class version : std::uint8_t {
 };
 
 }  // namespace _TMP_::ip
+
+template <>
+struct std::formatter<_TMP_::ip::version> {
+  template <class format_context>
+  constexpr auto parse(format_context& ctx) {
+    return ctx.begin();
+  }
+
+  template <class format_context>
+  constexpr auto format(const _TMP_::ip::version& version,
+                        format_context& ctx) const {
+    switch (version) {
+      case _TMP_::ip::version::kIpV4:
+        return std::format_to(ctx.out(), "IPv4");
+      case _TMP_::ip::version::kIpV6:
+        return std::format_to(ctx.out(), "IPv6");
+    }
+  }
+};
