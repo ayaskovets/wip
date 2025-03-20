4d98b0d48385c9e421162a021010370925cd535b
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:35 +0300

    add datetime module

---
+++ todo/src/datetime/serialization.cpp
@@ -0,0 +1,19 @@
+#include "datetime/serialization.hpp"
+
+namespace todo::datetime {
+
+std::string to_iso8601(std::chrono::system_clock::time_point time_point) {
+  constexpr auto kIso8601Fmt = "{}-{:0>2}-{:0>2}T{:0>2}:{:0>2}:{:0>2}.{:0>6}Z";
+
+  const std::time_t time = std::chrono::system_clock::to_time_t(time_point);
+  std::tm tm;
+  gmtime_r(&time, &tm);
+  const auto tm_msec =
+      time_point.time_since_epoch() -
+      std::chrono::system_clock::from_time_t(time).time_since_epoch();
+
+  return std::format(kIso8601Fmt, 1900 + tm.tm_year, tm.tm_mon + 1, tm.tm_mday,
+                     tm.tm_hour, tm.tm_min, tm.tm_sec, tm_msec.count());
+}
+
+}  // namespace todo::datetime
