d7e39044d863dbfadbd6a59853eef1c7219696b4
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:38 +0300

    add port class

--- _TMP_/include/ip/port.hpp
+++ _TMP_/include/ip/port.hpp
@@ -4,6 +4,15 @@
 
 namespace _TMP_::ip {
 
-using port = std::uint16_t;
+class port {
+ public:
+  port(std::uint16_t) noexcept;
+
+ public:
+  std::uint16_t as_bytes() const noexcept;
+
+ private:
+  std::uint16_t port_;
+};
 
 }  // namespace _TMP_::ip
