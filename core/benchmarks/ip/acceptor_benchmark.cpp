52a83915ec335e9f571337ea13478e2e366783a4
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:31 +0300

    add acceptor try_accept time estimation

---
+++ core/benchmarks/ip/acceptor_benchmark.cpp
@@ -0,0 +1,14 @@
+#include <benchmark/benchmark.h>
+
+#include "ip/acceptor.hpp"
+
+void BM_acceptor_try_accept(benchmark::State& state) {
+  const auto acceptor = core::ip::acceptor(
+      core::ip::address::kLocalhost(core::ip::version::kIpV4),
+      core::ip::port(9996));
+
+  for (const auto _ : state) {
+    benchmark::DoNotOptimize(acceptor.try_accept());
+  }
+}
+BENCHMARK(BM_acceptor_try_accept);
