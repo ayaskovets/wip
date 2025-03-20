5cd0063055c9b7047878ca4f0e75285d43599347
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:52:42 +0300

    add get handle methods to fd

--- core/src/io/fd.cpp
+++ core/src/io/fd.cpp
@@ -29,19 +29,23 @@ const fd& fd::kStderr() noexcept {
   return fd;
 }
 
-fd::fd(int fd) noexcept : fd_(fd) {}
+fd::fd(int fd) : fd_(fd) {
+  if (fd_ < 0) {
+    throw std::invalid_argument(std::format("invalid file descriptor: {}", fd));
+  }
+}
 
 fd::fd(const fd& that) {
-  if ((fd_ = ::dup(that.fd_)) == kSyscallError) {
-    throw std::runtime_error(
-        std::format("failed to clone file fd: {}", std::strerror(errno)));
+  if ((fd_ = ::dup(that.fd_)) == kSyscallError) [[unlikely]] {
+    throw std::runtime_error(std::format("failed to clone file descriptor: {}",
+                                         std::strerror(errno)));
   }
 }
 
 fd& fd::operator=(const fd& that) {
-  if (::dup2(that.fd_, fd_) == kSyscallError) {
-    throw std::runtime_error(
-        std::format("failed to clone file fd: {}", std::strerror(errno)));
+  if (::dup2(that.fd_, fd_) == kSyscallError) [[unlikely]] {
+    throw std::runtime_error(std::format("failed to clone file descriptor: {}",
+                                         std::strerror(errno)));
   }
   return *this;
 }
@@ -50,17 +54,16 @@ fd::fd(fd&& that) noexcept : fd_(std::exchange(that.fd_, kInvalidFd)) {}
 
 fd& fd::operator=(fd&& that) {
   if (fd_ != kInvalidFd &&
-      ::close(std::exchange(fd_, kInvalidFd)) == kSyscallError) {
-    throw std::runtime_error(
-        std::format("failed to close file fd: {}", std::strerror(errno)));
+      ::close(std::exchange(fd_, kInvalidFd)) == kSyscallError) [[unlikely]] {
+    throw std::runtime_error(std::format("failed to close file descriptor: {}",
+                                         std::strerror(errno)));
   }
-
   fd_ = std::exchange(that.fd_, kInvalidFd);
   return *this;
 }
 
 fd::~fd() noexcept {
-  if (fd_ != kInvalidFd) {
+  if (fd_ != kInvalidFd) [[unlikely]] {
     [[maybe_unused]] const auto _ = ::close(std::exchange(fd_, kInvalidFd));
   }
 }
@@ -72,10 +75,22 @@ bool fd::operator==(const fd& that) const noexcept {
 bool fd::operator!=(const fd& that) const noexcept { return !operator==(that); }
 
 void fd::close() {
-  if (fd_ != kInvalidFd && ::close(std::exchange(fd_, kInvalidFd)) == -1) {
+  if (fd_ == kInvalidFd) [[unlikely]] {
     throw std::runtime_error(
-        std::format("failed to close file fd: {}", std::strerror(errno)));
+        std::format("close() called on already closed file descriptor: {}",
+                    std::strerror(errno)));
   }
+  if (::close(std::exchange(fd_, kInvalidFd)) == kSyscallError) [[unlikely]] {
+    throw std::runtime_error(std::format("failed to close file descriptor: {}",
+                                         std::strerror(errno)));
+  }
+}
+
+int fd::get_fd() const {
+  if (fd_ == kInvalidFd) [[unlikely]] {
+    throw std::runtime_error("get_fd() called on a closed file descriptor");
+  }
+  return fd_;
 }
 
 }  // namespace core::io
