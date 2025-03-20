aef8a01bf659174ff0a9096d02a140c473a21e31
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:30 +0300

    refactor tests using test fixtures and parameters

--- _TMP_/tests/datetime_test.cpp
+++ _TMP_/tests/datetime_test.cpp
@@ -4,33 +4,28 @@
 
 namespace tests::logging {
 
-namespace datetime = _TMP_::datetime;
+class _TMP__datetime
+    : public ::testing::TestWithParam<
+          std::tuple<std::chrono::system_clock::time_point, std::string_view>> {
+};
 
-TEST(_TMP__datetime, as_iso8601) {
-  using namespace std::chrono;
-  const system_clock::time_point time_point =
-      sys_days{January / 9 / 2014} + 12h + 35min + 34s + 125ms + 322us;
-  const auto timestamp = "2014-01-09T12:35:34.125322Z";
-
-  EXPECT_EQ(datetime::as_iso8601(time_point), timestamp);
+TEST_P(_TMP__datetime, as_iso8601) {
+  const auto [time_point, string] = GetParam();
+  EXPECT_EQ(_TMP_::datetime::as_iso8601(time_point), string);
 }
 
-TEST(_TMP__datetime, as_iso8601_before_epoch) {
-  using namespace std::chrono;
-  const system_clock::time_point time_point =
-      sys_days{January / 9 / 1300} + 12h + 35min + 34s + 125ms + 322us;
-  const auto timestamp = "1300-01-09T12:35:34.125322Z";
-
-  EXPECT_EQ(datetime::as_iso8601(time_point), timestamp);
-}
-
-TEST(_TMP__datetime, as_iso8601_after_max_time) {
-  using namespace std::chrono;
-  const system_clock::time_point time_point =
-      sys_days{January / 9 / 3235} + 12h + 35min + 34s + 125ms + 322us;
-  const auto timestamp = "3235-01-09T12:35:34.125322Z";
-
-  EXPECT_EQ(datetime::as_iso8601(time_point), timestamp);
-}
+using namespace std::chrono_literals;
+INSTANTIATE_TEST_SUITE_P(
+    _TMP__datetime, _TMP__datetime,
+    ::testing::Values(
+        std::make_tuple(std::chrono::sys_days{std::chrono::January / 9 / 2014} +
+                            12h + 35min + 34s + 125ms + 322us,
+                        "2014-01-09T12:35:34.125322Z"),
+        std::make_tuple(std::chrono::sys_days{std::chrono::January / 9 / 1300} +
+                            12h + 35min + 34s + 125ms + 322us,
+                        "1300-01-09T12:35:34.125322Z"),
+        std::make_tuple(std::chrono::sys_days{std::chrono::January / 9 / 3235} +
+                            12h + 35min + 34s + 125ms + 322us,
+                        "3235-01-09T12:35:34.125322Z")));
 
 }  // namespace tests::logging
--- _TMP_/tests/logging_test.cpp
+++ _TMP_/tests/logging_test.cpp
@@ -4,57 +4,56 @@
 
 namespace tests::logging {
 
-namespace logging = _TMP_::logging;
-
 TEST(_TMP__logging, writer) {
   std::string buffer;
-  const logging::logger logger(
-      logging::level::kInfo, logging::renderer::kNoop(),
-      logging::writer(
-          [&buffer]([[maybe_unused]] logging::level level,
+  const _TMP_::logging::logger logger(
+      _TMP_::logging::level::kInfo, _TMP_::logging::renderer::kNoop(),
+      _TMP_::logging::writer(
+          [&buffer]([[maybe_unused]] _TMP_::logging::level level,
                     std::string_view message) { buffer = message; }));
 
-  logger(logging::level::kInfo, "{}", 42);
+  logger(_TMP_::logging::level::kInfo, "{}", 42);
   EXPECT_EQ(buffer, "42");
 }
 
 TEST(_TMP__logging, skip) {
   std::string buffer;
-  const logging::logger logger(
-      logging::level::kWarn, logging::renderer::kNoop(),
-      logging::writer([&buffer]([[maybe_unused]] logging::level level,
-                                [[maybe_unused]] std::string_view message) {
-        buffer.resize(buffer.size() + 1);
-      }));
+  const _TMP_::logging::logger logger(
+      _TMP_::logging::level::kWarn, _TMP_::logging::renderer::kNoop(),
+      _TMP_::logging::writer(
+          [&buffer]([[maybe_unused]] _TMP_::logging::level level,
+                    [[maybe_unused]] std::string_view message) {
+            buffer.resize(buffer.size() + 1);
+          }));
 
-  logger(logging::level::kDebug, "message");
+  logger(_TMP_::logging::level::kDebug, "message");
   EXPECT_TRUE(buffer.empty());
 
-  logger(logging::level::kInfo, "message");
+  logger(_TMP_::logging::level::kInfo, "message");
   EXPECT_TRUE(buffer.empty());
 
-  logger(logging::level::kWarn, "message");
+  logger(_TMP_::logging::level::kWarn, "message");
   EXPECT_EQ(buffer.size(), 1);
 
-  logger(logging::level::kError, "message");
+  logger(_TMP_::logging::level::kError, "message");
   EXPECT_EQ(buffer.size(), 2);
 }
 
 TEST(_TMP__logging, renderer) {
   std::string buffer;
-  const logging::logger logger(
-      logging::level::kInfo,
-      logging::renderer([]([[maybe_unused]] logging::level level,
-                           std::string_view fmt,
-                           std::format_args args) -> std::string {
+  const _TMP_::logging::logger logger(
+      _TMP_::logging::level::kInfo,
+      _TMP_::logging::renderer([]([[maybe_unused]] _TMP_::logging::level level,
+                                  std::string_view fmt,
+                                  std::format_args args) -> std::string {
         return std::vformat(fmt, std::move(args)) +
                std::vformat(fmt, std::move(args));
       }),
-      logging::writer(
-          [&buffer]([[maybe_unused]] logging::level level,
+      _TMP_::logging::writer(
+          [&buffer]([[maybe_unused]] _TMP_::logging::level level,
                     std::string_view message) { buffer = message; }));
 
-  logger(logging::level::kInfo, "{}", 42);
+  logger(_TMP_::logging::level::kInfo, "{}", 42);
   EXPECT_EQ(buffer, "4242");
 }
 
