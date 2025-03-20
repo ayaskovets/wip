076da21cecd38f6463a17c6e89000b6874acb492
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:31 +0300

    move static_assert to template requirement

--- core/include/utils/declarative.hpp
+++ core/include/utils/declarative.hpp
@@ -6,10 +6,8 @@
 namespace core::utils {
 
 template <std::invocable T>
+  requires(noexcept(std::declval<T>()()))
 class scope_exit final {
- private:
-  static_assert(noexcept(std::declval<T>()()));
-
  public:
   constexpr explicit scope_exit(T&& finally)
       : finally_(std::forward<T>(finally)) {}
