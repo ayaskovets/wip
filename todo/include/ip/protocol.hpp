1284caeb5b950d1019edd3b87438ed330ab62962
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:30 +0300

    remove nodiscard

--- todo/include/ip/protocol.hpp
+++ todo/include/ip/protocol.hpp
@@ -15,13 +15,13 @@ enum class protocol : std::uint8_t {
 template <>
 struct std::formatter<todo::ip::protocol> {
   template <class format_context>
-  [[nodiscard]] constexpr auto parse(format_context& ctx) {
+  constexpr auto parse(format_context& ctx) {
     return ctx.begin();
   }
 
   template <class format_context>
-  [[nodiscard]] constexpr auto format(const todo::ip::protocol& protocol,
-                                      format_context& ctx) const {
+  constexpr auto format(const todo::ip::protocol& protocol,
+                        format_context& ctx) const {
     switch (protocol) {
       case todo::ip::protocol::kTcp:
         return std::format_to(ctx.out(), "TCP");
