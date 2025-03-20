372a0e398c7eb682276b1bfdb1996bedb2892bdf
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-03-20 21:57:30 +0300

    strict includes and constexpr additions

--- _TMP_/include/utils/mixins.hpp
+++ _TMP_/include/utils/mixins.hpp
@@ -3,15 +3,15 @@
 namespace _TMP_::utils {
 
 struct non_copyable {
-  non_copyable() = default;
-  non_copyable(const non_copyable&) = delete;
-  non_copyable& operator=(const non_copyable&) = delete;
+  constexpr non_copyable() = default;
+  constexpr non_copyable(const non_copyable&) = delete;
+  constexpr non_copyable& operator=(const non_copyable&) = delete;
 };
 
 struct non_movable {
-  non_movable() = default;
-  non_movable(non_copyable&&) = delete;
-  non_movable& operator=(non_movable&&) = delete;
+  constexpr non_movable() = default;
+  constexpr non_movable(non_copyable&&) = delete;
+  constexpr non_movable& operator=(non_movable&&) = delete;
 };
 
 }  // namespace _TMP_::utils
