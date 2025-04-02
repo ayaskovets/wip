d10deb0259f673f097eb40909e8a6ff344a1e254
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-04-02 22:46:34 +0300

    remove external usages of get_fd

--- core/tests/io/fd_test.cpp
+++ core/tests/io/fd_test.cpp
@@ -11,19 +11,15 @@ TEST(io_fd, size) {
 
 TEST(io_fd, manual_close) {
   core::io::fd fd(core::io::fd::kStdin());
-  EXPECT_NO_THROW(fd.get_fd());
   EXPECT_NO_THROW(fd.close());
   EXPECT_ANY_THROW(fd.close());
   EXPECT_ANY_THROW(fd.close());
-  EXPECT_ANY_THROW(fd.get_fd());
 }
 
 TEST(io_fd, transfer_ownership) {
   core::io::fd moved_from(core::io::fd::kStdin());
   core::io::fd moved_to = std::move(moved_from);
   EXPECT_NE(moved_from, moved_to);
-  EXPECT_ANY_THROW(moved_from.get_fd());
-  EXPECT_NO_THROW(moved_to.get_fd());
   EXPECT_ANY_THROW(moved_from.close());
   EXPECT_NE(moved_from, moved_to);
   EXPECT_NO_THROW(moved_to.close());
@@ -33,8 +29,6 @@ TEST(io_fd, transfer_ownership) {
 TEST(io_fd, duplicate) {
   core::io::fd original(core::io::fd::kStdin());
   core::io::fd copy(original);
-  EXPECT_NO_THROW(original.get_fd());
-  EXPECT_NO_THROW(copy.get_fd());
   EXPECT_NE(original, copy);
   EXPECT_NO_THROW(original.close());
   EXPECT_NO_THROW(copy.close());
