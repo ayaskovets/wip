e6e39185161f5960f12941775a55e323c70c0b00
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-04-06 20:10:34 +0300

    remove outdated ip implementations

--- core/src/net/ip/udp/socket.cpp
+++
@@ -1,8 +0,0 @@
-#include "net/ip/udp/socket.hpp"
-
-namespace core::net::ip::udp {
-
-socket::socket(net::ip::version version)
-    : net::ip::socket(net::ip::protocol::kUdp, version) {}
-
-}  // namespace core::net::ip::udp
