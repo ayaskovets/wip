fb2072ccd7962dfee9d215d593b5d6fd24ee9f4f
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-04-19 01:16:49 +0300

    fix

--- core/include/utils/constants.hpp
+++ core/include/utils/constants.hpp
@@ -5,6 +5,6 @@
 namespace core::utils {
 
 constexpr const std::size_t kCacheLineSize = 64;
-constexpr std::size_t kRuntimeCapacity = 0;
+constexpr const std::size_t kRuntimeCapacity = 0;
 
 }  // namespace core::utils
