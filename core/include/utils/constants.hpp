b997cd4964922ff5344ac0742048a702566f230a
Andrei Yaskovets <ayaskovets@users.noreply.github.com>
2025-04-25 09:50:53 +0300

    make kDynamicCapacity template

--- core/include/utils/constants.hpp
+++ core/include/utils/constants.hpp
@@ -5,6 +5,8 @@
 namespace core::utils {
 
 constexpr const std::size_t kCacheLineSize = 64;
-constexpr const std::size_t kRuntimeCapacity = 0;
+
+template <std::unsigned_integral T>
+constexpr const T kDynamicCapacity = 0;
 
 }  // namespace core::utils
